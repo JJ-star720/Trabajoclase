7856753965af3df39291065bf18e606241ad8b33
Usuario <Usuario@DESKTOP-E9D67EJ>
2025-05-25 22:08:50 -0600

    Agregar archivos de proyecto.

---
+++ Models/Persona.vb
@@ -0,0 +1,11 @@
+﻿Public Class Persona
+    Private _nombre As String
+    Private _ápellido As String
+    Private _edad As String
+    Private _dirreccion As String
+    Private _telefono As String
+    Private _correo As String
+
+
+
+End Class
