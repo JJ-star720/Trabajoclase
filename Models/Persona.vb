d9f3e3bfb5c2d110ac628860f5cbb463b6650bf4
JUAN JOSE VELEZ CAYOLA <jvelezc@edu.uc.ac.cr>
2025-06-12 13:36:45 -0600

    Encapsulamiento de persona

--- Models/Persona.vb
+++ Models/Persona.vb
@@ -8,4 +8,57 @@
 
 
 
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Public Property Ápellido As String
+        Get
+            Return _ápellido
+        End Get
+        Set(value As String)
+            _ápellido = value
+        End Set
+    End Property
+
+    Public Property Edad As String
+        Get
+            Return _edad
+        End Get
+        Set(value As String)
+            _edad = value
+        End Set
+    End Property
+
+    Public Property Dirreccion As String
+        Get
+            Return _dirreccion
+        End Get
+        Set(value As String)
+            _dirreccion = value
+        End Set
+    End Property
+
+    Public Property Telefono As String
+        Get
+            Return _telefono
+        End Get
+        Set(value As String)
+            _telefono = value
+        End Set
+    End Property
+
+    Public Property Correo As String
+        Get
+            Return _correo
+        End Get
+        Set(value As String)
+            _correo = value
+        End Set
+    End Property
 End Class
